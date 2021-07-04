af0fcd9a7a1b91986e9b9a8ed3a89bfa1c4075ba
qwertscid223 <86884083+qwertscid223@users.noreply.github.com>
2021-07-04 08:57:23 +0530

    Update send.py

--- send.py
+++ send.py
@@ -74,7 +74,7 @@ def Track() :
 def update():
     stuff_to_update = ['send.py', 'Run.sh', '.version']
     for fl in stuff_to_update:
-        dat = urllib.request.urlopen("https://raw.githubusercontent.com/qwertscid223/cid-anon-sms/master/" + fl).read()
+        dat = urllib.request.urlopen("https://raw.githubusercontent.com/qwertscid223/cid-anon-sms/main/" + fl).read()
         file = open(fl, 'wb')
         file.write(dat)
         file.close()
@@ -92,7 +92,7 @@ except Exception:
     input('Exiting....\n Press Enter To Exit....')
     exit()
 print('\tChecking For Updates...')
-ver = urllib.request.urlopen("https://raw.githubusercontent.com/qwertscid223/cid-anon-sms/master/.version").read().decode('utf-8')
+ver = urllib.request.urlopen("https://raw.githubusercontent.com/qwertscid223/cid-anon-sms/main/.version").read().decode('utf-8')
 verl = ''
 try:
     verl = open(".version", 'r').read()
@@ -106,7 +106,7 @@ print("Congratulation")
 print("Your Version is Up-To-Date")
 print('\n\tStarting cid-anon-sms...\n')
 try:
-    noti = urllib.request.urlopen("https://raw.githubusercontent.com/qwertscid223/cid-anon-sms/master/.notify").read().decode('utf-8')
+    noti = urllib.request.urlopen("https://raw.githubusercontent.com/qwertscid223/cid-anon-sms/main/.notify").read().decode('utf-8')
     if len(noti) > 10:
         print('\nNotification : ' + noti + '\n')
 except Exception:
